b8e03a2cc7058924b61515df104fe2d3656a99cc
Osama Salem <oafsalem2000@gmail.com>
2020-10-12 08:45:28 +0300

    finished lab05 work

---
+++ Lab Tasks/Lab 05 Recursion/DLL.java
@@ -0,0 +1,142 @@
+
+//import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+//****************************  DLL.java  *******************************
+//                  generic doubly linked list class
+
+public class DLL<T> {
+    private DLLNode<T> head, tail;
+    public DLL() {
+        head = tail = null;
+    }
+    public boolean isEmpty() {
+        return head == null;
+    }
+    public void setToNull() {
+        head = tail = null;
+    }
+    public T firstEl() {
+        if (head != null)
+             return head.info;
+        else return null;
+    }
+    public DLLNode<T> getHead(){
+        return this.head;
+    }
+    public void addToHead(T el) {
+        if (head != null) {
+             head = new DLLNode<T>(el,head,null);
+             head.next.prev = head;
+        }
+        else head = tail = new DLLNode<T>(el);
+    }
+    public void addToTail(T el) {
+        if (tail != null) {
+             tail = new DLLNode<T>(el,null,tail);
+             tail.prev.next = tail;
+        }
+        else head = tail = new DLLNode<T>(el);
+    }
+    public T deleteFromHead() {
+        if (isEmpty()) 
+             return null;
+        T el = head.info;
+        if (head == tail)   // if only one node on the list;
+             head = tail = null;
+        else {              // if more than one node in the list;
+             head = head.next;
+             head.prev = null;
+        }
+        return el;
+    }
+    public T deleteFromTail() {
+        if (isEmpty()) 
+             return null;
+        T el = tail.info;
+        if (head == tail)   // if only one node on the list;
+             head = tail = null;
+        else {              // if more than one node in the list;
+             tail = tail.prev;
+             tail.next = null;
+        }
+        return el;
+    }
+    public void printAll() { 
+        for (DLLNode<T> tmp = head; tmp != null; tmp = tmp.next)
+             System.out.print(tmp.info + " ");
+    }
+    public T find(T el) {
+        DLLNode<T> tmp;
+        for (tmp = head; tmp != null && !tmp.info.equals(el); tmp = tmp.next);
+        if (tmp == null)
+             return null;
+        else return tmp.info;
+    }
+    public void printReverse() {
+        if (head == tail){ // if the list has one item or no items
+            if(head == null){
+                throw new RuntimeException("This DLL IS Empty");
+            }
+            System.out.println(head.info);
+        }
+        else{
+            DLLNode<T> tmp = tail;
+            while (tmp.prev != null){
+                System.out.print(tmp.info + " ");
+                tmp = tmp.prev;
+            }
+            System.out.println(this.head.info);
+        }
+    }
+    public void delete7(){
+        if (head == tail){
+            if(head ==null){
+            throw new RuntimeException("This DLL IS Empty");
+            }
+            else{
+                head = tail = null;
+            }
+        }
+        else{
+            int count = 1; //from one since im already in the head
+            boolean found = false;
+            DLLNode<T> tmp=head;
+            while (!found){ // while i didnt reach the 7th element
+                while(tmp.next != null){ // loop to go forward
+                    if(count == 7){ // if we reach element 7;
+                        found = true;
+                        break;
+                    }
+                    else{ //still not there
+                        tmp = tmp.next;
+                        count++;
+                    }
+                    }
+                if (!found && tmp == tail){ // while we still didnt find anything
+                    while(tmp.prev != null){ // loop to go backwards
+                        if(count == 7){ //if we reach element 7;
+                            found = true;
+                            break;
+                        }
+                        else{ // still not there
+                            tmp = tmp.prev;
+                            count++;
+                        }
+                    }
+                }
+            } //if found.
+            if(tmp == head){
+                head = tmp.next;
+                head.prev = null;
+            }
+            else if(tmp == tail){
+                tail = tmp.prev;
+                tail.next = null;
+            }
+            else {
+                tmp.prev.next = tmp.next;
+                tmp.next.prev =tmp.prev;
+            }
+        }
+    }
+}
--- Lab Tasks/Lab 05 Recursion/Solution.java
+++ Lab Tasks/Lab 05 Recursion/Solution.java
@@ -1,10 +1,18 @@
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         String[] array = {"this", "is", "I", "My name is bla bla bla", "bla bla bla"};
         System.out.println(findSmallest(array, 0));
         System.out.println(findLongest(array, 0));
         System.out.println(putCommas(1237811234));
-
+        DLL<Integer> test2 = new DLL<Integer>();
+		for(int i = 0; i < 10; i++) {
+			test2.addToTail(i);
+        }
+        test2.printAll();
+        System.out.println();
+        deleteKth(4, test2.getHead(), 0);
+        test2.printAll();
     }
 
     public static String findSmallest(String[] array, int index){
@@ -32,6 +40,20 @@ public class Solution {
             return x + "";
         }
         return putCommas(x/1000) + String.format(",%3d",(x%1000));
+    }
+    public static DLLNode<Integer> deleteKth(int k, DLLNode<Integer> node, int count) throws Exception {
+        if(k == count){
+            if (node.next != null){
+                node.next.prev = node.prev;
+            }
+            node.prev.next = node.next;
+            return node;
+        }
+        if(node.next == null)
+            throw new Exception("Error: K is out of the list");
+        
+        
+        return deleteKth(k, node.next, ++count);
     }
 
 
